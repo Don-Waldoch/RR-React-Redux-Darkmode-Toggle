2cb1f316b65c41932ab0ff41585a2527d7bd9334
unknown <waldoch.don@gmail.com>
2022-04-25 10:27:58 -0500

    Middleware

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { setID, incrementID, decrementID, resetState, fetchData } from './features/dataSlice'
+import { useEffect } from 'react';
 
-function App() {
-  // your logic goes here!
+function App(props) {
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.data)
 
+  const displayImage = () => {
+    if (data.apiData.primaryImage || data.apiData.title) {
+      return <img style={{'width': '25%'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p>Image Placeholder</p>
+    }
+  }
+
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [props.objectID, dispatch])
+  
   return (
     <div className="App">
       <div>
-        <button onClick={() => {}}>Trigger Thunk</button>
-        <button onClick={() => {}}>Clear</button>
-        <button onClick={() => {}}>Next</button>
-        <button onClick={() => {}}>Back</button>
+        <button onClick={() => dispatch(fetchData())}>Trigger Thunk</button>
+        <button onClick={() => dispatch(resetState())}>Clear</button>
+        <button onClick={() => dispatch(incrementID())}>Next</button>
+        <button onClick={() => dispatch(decrementID())}>Back</button>
       </div>
-      <input onChange={(e) => { }} />
       <div>
-        {/* Once you have plugged everything in, render the image here! */}
+        <input onChange={(e) => dispatch(setID(Number(e.target.value)))}/>
+      </div>
+      <div>
+        {data.objectID}
+        <p></p>
+        {displayImage()}
       </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => ({ objectID: state.data.objectID })
+export default connect(mapStateToProps)(App)
